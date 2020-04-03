9a49423a7ebf6f9310132d0e98f449d78bbdfcfd
Laura <lolacheng86@gmail.com>
2020-04-03 23:07:22 +0800

    Add login feature using passport and express-session

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
 
 const db = require('./models')
 const Todo = db.Todo
@@ -17,6 +19,22 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // mathod-override
 app.use(methodOverride('_method'))
+
+// session 
+app.use(session({
+    secret: 'sequelize',
+    resave: false,
+    saveUninitialized: false
+}))
+
+// use passport
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport) 
+app.use((req, res, next) => {
+    res.locals.user = req.user
+    next()
+})
 
 // add routers
 app.use(require('./routes/home'))
--- routes/user.js
+++ routes/user.js
@@ -10,14 +10,20 @@ const User = db.User
 router.get('/login', (req, res) => {
     res.render('login')
 })
+
 // 登入檢查
-router.post('/login', (req, res) => {
-    res.send('login')
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+    })(req, res, next)
 })
+
 // 註冊頁面
 router.get('/register', (req, res) => {
     res.render('register')
 })
+
 // 註冊檢查
 router.post('/register', (req, res) => {
     const { name, email, password, password2 } = req.body
@@ -39,16 +45,16 @@ router.post('/register', (req, res) => {
             newUser
             .save()
             .then(user => {
-                res.redirect('/')                   // 新增完成導回首頁
+                res.redirect('/')       // 新增完成導回首頁
             })
             .catch(err => console.log(err))
         }
     })
 })
+
 // 登出
 router.get('/logout', (req, res) => {
     res.send('logout')
 })
-
 
 module.exports = router
